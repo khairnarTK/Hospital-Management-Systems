7fdedbfd192fe6e41a4c50b45275280e07b0f7bf
Tushar <khairnar.tk@gmail.com>
2017-04-06 19:28:44 +0530

    Initial commit

---
+++ HMS/HMS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HMS.Web.Startup))]
+namespace HMS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
