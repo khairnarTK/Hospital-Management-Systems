f1d13fc34251088e3c211080963f5c5f1a9c9ba4
YOGESHVEDPATHAK333 <yogeshvedpathak333@gmail.com>
2017-04-22 17:56:38 +0530

    " 1.Crud Opeartion For (ManagePatient) Done 2.Crud Opeartion For (ManagePrescription) Done 3.Crud Opearetion For (Vistor Pass) Done "

--- HMS/HMS.Web/Models/HmsContext.cs
+++ HMS/HMS.Web/Models/HmsContext.cs
@@ -6,13 +6,16 @@ using System.Web;
 
 namespace HMS.Web.Models
 {
-    public class HmsContext:DbContext
+    public class HmsContext : DbContext
     {
         public DbSet<PatientRegistrations> PatientRegistrationes { get; set; }
-        public DbSet<PatientSurgeryReservation> PatientSurgeryReservations { get; set;}
-        public DbSet<OpeartionTheatre> OpeartionTheatres { get; set;}
-        public DbSet<ManageReception> ManageReceptions { get; set;}
-        public DbSet<ManageDoctors> ManageDoctors { get; set;}
-        public DbSet<ManageDoctorsDepartment> ManageDoctorsDepartments {get;set;}
+        public DbSet<PatientSurgeryReservation> PatientSurgeryReservations { get; set; }
+        public DbSet<OpeartionTheatre> OpeartionTheatres { get; set; }
+        public DbSet<ManageReception> ManageReceptions { get; set; }
+        public DbSet<ManageDoctors> ManageDoctors { get; set; }
+        public DbSet<ManageDoctorsDepartment> ManageDoctorsDepartments { get; set; }
+        public DbSet<ManagePatient> ManagePatients { get; set; }
+        public DbSet<ManagePrescription> ManagePrescriptions { get; set; }
+        public DbSet<ManageVisitorPass> ManageVisitorPass { get; set;}
     }
 }
