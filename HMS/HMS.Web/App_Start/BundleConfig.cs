6802c2391a6b7ecf675d82fef8ae71e934d3bd40
Tushar <khairnar.tk@gmail.com>
2017-05-06 19:39:38 +0530

    initial theme setup completed

--- HMS/HMS.Web/App_Start/BundleConfig.cs
+++ HMS/HMS.Web/App_Start/BundleConfig.cs
@@ -14,18 +14,13 @@ namespace HMS.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/bootstrap/js/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap/css/bootstrap.min.css",
+                      "~/Content/dist/css/skins/_all-skins.min.css",
+                      "~/Content/dist/css/AdminLTE.min.css"));
         }
     }
 }
